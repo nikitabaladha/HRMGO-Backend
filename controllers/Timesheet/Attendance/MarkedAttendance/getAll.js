a65e088ec0c880c0339ac01990e82570564d280e
nikitabaladha <nikitabaladha1996@gmail.com>
2024-11-13 18:37:27 +0530

    Marked Attendance Get All api done for showing record according to date department and branch

--- controllers/Timesheet/Attendance/MarkedAttendance/getAll.js
+++ controllers/Timesheet/Attendance/MarkedAttendance/getAll.js
@@ -1,48 +1,87 @@
+const mongoose = require("mongoose");
 const MarkedAttendance = require("../../../../models/MarkedAttendance");
-const moment = require("moment"); // To format the dates
-
+const moment = require("moment");
 async function getAll(req, res) {
   try {
-    // Populate the employeeId with the name field from the Employee model
-    const markedAttendance = await MarkedAttendance.find().populate(
-      "employeeId",
-      "name"
-    );
+    // Extract query parameters
+    const { branch, department, date, type } = req.query;
 
-    // Map the response data to include all required fields with employee name and formatted times
-    const markedAttendanceData = markedAttendance.map((attendance) => {
-      const employeeName = attendance.employeeId.name;
+    // Build query filter object
+    const filter = {};
 
-      // Format date and times using moment.js
-      const formattedDate = moment(attendance.date).format("MMM D, YYYY");
-      const formattedClockIn = moment(attendance.clockIn).format("h:mm A");
-      const formattedClockOut = moment(attendance.clockOut).format("h:mm A");
-      const formattedLate = moment.utc(attendance.late).format("HH:mm:ss");
-      const formattedEarlyLeaving = moment
-        .utc(attendance.earlyLeaving)
-        .format("HH:mm:ss");
-      const formattedOvertime = moment
-        .utc(attendance.overtime)
-        .format("HH:mm:ss");
+    // Filter by branch if provided
+    if (branch) filter["branchId"] = new mongoose.Types.ObjectId(branch);
 
-      return {
-        employeeName,
-        date: formattedDate,
-        status: attendance.status,
-        clockIn: formattedClockIn,
-        clockOut: formattedClockOut,
-        late: formattedLate,
-        earlyLeaving: formattedEarlyLeaving,
-        overtime: formattedOvertime,
-      };
-    });
+    // // Filter by department if provided
+    if (department)
+      filter["departmentId"] = new mongoose.Types.ObjectId(department);
+
+    let dateFilter = {};
+
+    if (date) {
+      const dateFilterFor = type === "Daily" ? "day" : "month";
+      const startOfMonth = moment(date).startOf(dateFilterFor).toISOString();
+      const endOfMonth = moment(date).endOf(dateFilterFor).toISOString();
+      dateFilter.date = { $gte: startOfMonth, $lte: endOfMonth };
+    }
+
+    // Populate the employee details with branch and department
+    const markedAttendance = await MarkedAttendance.find(dateFilter)
+      .populate({
+        path: "employeeId", // Populate the employeeId field
+        select: "name branchId departmentId", // Fields to select from the employee document
+        match: { ...filter },
+        populate: [
+          { path: "branchId" }, // Populate the branchId field
+          { path: "departmentId" }, // Populate the departmentId field
+        ],
+      })
+      .lean()
+      .exec();
+
+    // Map the response data to include all required fields with employee details
+    const markedAttendanceData = markedAttendance
+      .filter((attendance) => attendance.employeeId != null)
+      .map((attendance) => {
+        const employeeName = attendance.employeeId.name;
+        const branchName = attendance.employeeId.branchId.branchName;
+        const departmentName =
+          attendance.employeeId.departmentId.departmentName;
+
+        // Format date and times using moment.js
+        const formattedDate = moment(attendance.date).format("MMM D, YYYY");
+        const formattedClockIn = moment(attendance.clockIn).format("h:mm A");
+        const formattedClockOut = moment(attendance.clockOut).format("h:mm A");
+        const formattedLate = moment.utc(attendance.late).format("HH:mm:ss");
+        const formattedEarlyLeaving = moment
+          .utc(attendance.earlyLeaving)
+          .format("HH:mm:ss");
+        const formattedOvertime = moment
+          .utc(attendance.overtime)
+          .format("HH:mm:ss");
+
+        return {
+          employeeName,
+          branchName,
+          departmentName,
+          date: formattedDate,
+          status: attendance.status,
+          clockIn: formattedClockIn,
+          clockOut: formattedClockOut,
+          late: formattedLate,
+          earlyLeaving: formattedEarlyLeaving,
+          overtime: formattedOvertime,
+        };
+      });
+
+    console.log(markedAttendanceData);
 
     return res.status(200).json({
-      message: "MarkedAttendance retrieved successfully!",
+      message: "Marked Attendance retrieved successfully!",
       data: markedAttendanceData,
     });
   } catch (error) {
-    console.error("Error retrieving markedAttendance:", error);
+    console.error("Error retrieving marked attendance:", error);
     return res.status(500).json({
       message: "Failed to retrieve marked attendance.",
       error: error.message,
